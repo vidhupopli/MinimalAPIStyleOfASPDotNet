ec1dda13086f25906992f915899da74474a419c9
Vidhu Popli <vidhu.popli@woodmac.com>
2025-03-16 04:42:15 +0530

    Using path param and query param

--- MinimalAPI/Controllers/ShirtsController.cs
+++ MinimalAPI/Controllers/ShirtsController.cs
@@ -6,22 +6,29 @@ namespace MinimalAPI.Controllers
     [Route("api/shirts")]
     public class ShirtsController: ControllerBase
     {
+        // Query params means you do not use separate routes. You utilize existing ones.
+        // Here this is a generic route.
         [HttpGet]
-        public string GetAllShirts()
+        public string GetShirts([FromQuery] string? Id)
         {
-            return "All the shirts!";
+            return string.IsNullOrEmpty(Id) ? "All the shirts!" : $"Shirt fetched from query: {Id}";
+        }
+
+        [HttpGet("{Id}")]
+        public string GetSpecificShirt([FromRoute] int Id, [FromQuery] string? color)
+        {
+            if (!string.IsNullOrEmpty(color))
+            {
+                return $"route param: {Id} | queryParam: {color}";
+            }
+
+            return $"Route param: ${Id}";
         }
         
         [HttpPost]
         public string CreateSpecificShirt()
         {
             return "Creating Shirt";
-        }
-
-        [HttpGet("{Id}")] // In case you do not want to use Route attribute.
-        public string GetSpecificShirt(int Id)
-        {
-            return $"Specific Shirt: {Id}";
         }
 
         [HttpPut]
